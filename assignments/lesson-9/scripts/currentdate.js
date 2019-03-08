9bca5942c614b5c10cf3f65cd5c6e26a75e7f8f9
kinamichie <mic18019@byui.edu>
2019-03-07 22:18:24 -0900

    update week 9

---
+++ assignments/lesson-9/scripts/currentdate.js
@@ -0,0 +1,5 @@
+
+var d = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+document.getElementById("output").innerHTML = days[d.getDay()] +", "+ d.getDate()+" "+ months[d.getMonth()] + " " + d.getFullYear();
