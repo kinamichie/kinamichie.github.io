23e2b10ab710424e4debecf5e6528be41084b3cf
kinamichie <mic18019@byui.edu>
2019-03-16 20:26:04 -0800

    update week 10

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -10,23 +10,28 @@ forecastRequest.onload = function() {
 
        
 
-    /*
+    var number= 1;
+
     for (var i=0; i<forecastData.list.length; i+=8) {    
-    var myDay = document.createElement('h2');
-    var myicon = document.createElement('img'); 
-    var mytemp = document.createElement('p');
-    myDay.textContent=forecastData.list[i].dt_txt
-    myicon.img = forecastData.list[i].weather[i].icon;  
-    mytemp.textContent= forecastData.list[i].main.temp;
-    
+        document.getElementById('hight' + number).innerHTML=forecastData.list[i].main.temp + "&deg" + "F  ";
+        number++;
+
+        /*var d = new Date();
+        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        document.getElementById("demo").innerHTML = days[d.getDay()];
+        document.getElementById('today' + number).innerHTML = forecastData.list[i].dt_txt;
+
+.
+
+
+        /*let icon = "http://openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon + ".png";
+     let des = weatherData.weather[0].description;
+
+        document.getElementById('des' + icon).innerHTML=forecastData.list[i].weather[i].icon;
+        icon++;
+
+        document.getElementById('cc-img').setAttribute('src', icon);
+        document.getElementById('cc=img').setAttribute('alt', des);*/
+
     }
-
-    
-    /*document.getElementById('des').innerHTML =  
-    document.getElementById('today'),innerHTML = forecastData.list[i].dt_txt; 
-    document.getElementById('hight').innerHTML = forecastData.list[i].main.temp_max;
-    document.getElementById('des').setAttribute('src', icon);
-    document.getElementById('des').setAttribute('alt', des);
-
-    }  */
 }
