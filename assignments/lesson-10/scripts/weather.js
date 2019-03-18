140e740f793b937eeabc4c759b883e87abd4931d
kinamichie <mic18019@byui.edu>
2019-03-18 13:54:59 -0800

    update

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -12,10 +12,10 @@ forecastRequest.onload = function() {
 
     var number= 1;
     //var date=1;
-    icon=1;
+    var icon=1;
 
     for (var i=0; i<forecastData.list.length; i+=8) {    
-        document.getElementById('hight' + number).innerHTML=forecastData.list[i].main.temp + "&deg" + "F  ";
+        document.getElementById('hight' + number).innerHTML=forecastData.list[i].main.temp + "&deg" + "F";
         number++;
    
 
@@ -27,20 +27,19 @@ forecastRequest.onload = function() {
         weekday[(date.getDate() + 2) % 7];
     
         
-        document.getElementById('today' + date).innerHTML = forecastData.list[i].dt_txt;
+        document.getElementById('day' + date).innerHTML = forecastData.list[i].dt_txt;
         date++;
 
     }*/
 
 
-        /*icon.src = + forecastData.list[i].weather[i].icon + ".png";
-        let des = weatherData.weather[i].description;*/
-
-        document.getElementById('cc-img' + icon).innerHTML= "https://openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon +"png";
+        //let des = forecastData.list[i].weather[0].description;
+        
+        document.getElementById('cc-img' + icon).innerHTML= "https//openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon +"png";
         icon++;
 
-        /*document.getElementById('cc-img').setAttribute('src', icon);
-        document.getElementById('cc=img').setAttribute('alt', des);*/
+        //document.getElementById('cc-img').setAttribute('src', icon);
+        //document.getElementById('cc=img').setAttribute('alt', des);
 
     }
 }
