ae01b8eda8141dca6697a0d36459e01a5227d4a0
kinamichie <mic18019@byui.edu>
2019-03-19 22:20:34 -0800

    update

--- assignments/lesson-11/scripts/weather.js
+++ assignments/lesson-11/scripts/weather.js
@@ -8,58 +8,45 @@ forecastRequest.onload = function() {
    
     console.log(forecastData);       
 
-    var index=0;
-    var day =(new Date().getDay()+1)%7;
-    var section=document.getElementById('forecast');
-
-
-    while (day != new Date(forecastData.list[index].dt_txt).getDay())index++;
-    while (!forecastData.list[index].dt_txt.includes("18:00:00"))index++;
-    for (var today=0; today<5; today++) {
-
-    var article=document.createElement('article');
-    var header=document.createElement('h4');
-    var image=document.createElement('img');
-    var para=document.createElement('p');
     
+    var number=1;
+    for (var i=0; i<forecastData.list.length; i++) { 
+        forecastData.list[i].dt_txt.includes("18:00:00");
+        
+        if (true){
+            var weekDay = new Date().getDay()+number;
+            if(weekDay > 6){
+                weekDay = weekDay%7;
+            }            
+    
+            switch (weekDay) {
+                case 0:weekDay = "Sun";break;
+                case 1:weekDay = "Mon";break;
+                case 2:weekDay = "Tue";break;
+                case 3:weekDay = "Wed";break;
+                case 4:weekDay = "Thu";break;
+                case 5:weekDay = "Fri";break;
+                case 6:weekDay = "Sat";break;
+                default: weekDay="MON";break;
+            }
+           
+                       
+        //days
+        document.getElementById('day' + number).innerHTML= weekDay;          
 
-    header.textContent=GetDayName(day);
-    image.src="http://openweathermap.org/img/w/" + forecastData.list[index].weather[0].icon + ".png";
-    image.alt=forecastData.list[index].weather[0].description;
-    para.textContent=Math.round(forcastData.list[].main.temp) + "&deg" + "F";
+        //weather icon
+        let icon = "http://openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon + ".png";
+        //let des = forecastData.list[i].weather[0].description;      
+        
+        document.getElementById('cc-img' + number).setAttribute('src', icon);
+        //document.getElementById('cc=img' + number).setAttribute('alt', des);
 
-    article.appendChild(header);
-    article.appendChild(image);
-    article.appendChild(para);
+        //temp 
+        document.getElementById('hight' + number).innerHTML=forecastData.list[i].main.temp + "&deg" + "F";
+        number++;             
+    }
+    else{}
+    } 
+    }
 
-    section.appendChild(article);
-    day=(day+1)%7;
-    index+=8;
-}
-}
-
-//names of week
-   function GetDayName(aDay) 
-       {
-        var result = "MON";
-        switch (aDay % 7) {
-        case 0:result = "Sun";
-        break;
-        case 1:result= "Mon";
-        break;
-        case 2:result = "Tue";
-        break;
-        case 3:result = "Wed";
-        break;
-        case 4:result = "Thu";
-        break;
-        case 5:result = "Fri";
-        break;
-        case 6:result = "Sat";
-        break;
-
-}        
-        return result;
-
-}
     
