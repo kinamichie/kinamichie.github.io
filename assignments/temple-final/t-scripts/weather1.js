925c3c3db9f49535a74cf42d6f064d4431c74673
kinamichie <mic18019@byui.edu>
2019-04-09 17:03:06 -0800

    update

--- assignments/temple-final/t-scripts/weather1.js
+++ assignments/temple-final/t-scripts/weather1.js
@@ -1,14 +1,19 @@
-let weatherRequest = new XMLHttpRequest();
+let moweatherRequest = new XMLHttpRequest();
 let apiURLstring7 = 'https://api.openweathermap.org/data/2.5/forecast?id=4943629&units=imperial&APPID=bf97aaaa16a02c56f0db0c5d12eb5eed';
-weatherRequest.open('Get', apiURLstring7, true);
-weatherRequest.send();
+moweatherRequest.open('Get', apiURLstring7, true);
+moweatherRequest.send();
 
-weatherRequest.onload = function() {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+moweatherRequest.onload = function() {
+    let weatherData = JSON.parse(moweatherRequest.responseText);
     //console.log(weatherData);
    
 
     
     document.getElementById('temp1').innerHTML = weatherData.list[0].main.temp.toFixed(0) +"&#8457";   
+    
+    let icon = "https://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";
+    let des = weatherData.list[0].weather[0].description;  
 
+    document.getElementById('cc-img2').setAttribute('src', icon);
+    document.getElementById('cc-img2').setAttribute('alt', des);
 }
--- assignments/temple-final/t-scripts/weather2.js
+++ assignments/temple-final/t-scripts/weather2.js
@@ -1,14 +1,21 @@
-let weatherRequest = new XMLHttpRequest();
+let poweatherRequest = new XMLHttpRequest();
 let apiURLstring8 = 'https://api.openweathermap.org/data/2.5/forecast?id=4720131&units=imperial&APPID=bf97aaaa16a02c56f0db0c5d12eb5eed';
-weatherRequest.open('Get', apiURLstring8, true);
-weatherRequest.send();
+poweatherRequest.open('Get', apiURLstring8, true);
+poweatherRequest.send();
 
-weatherRequest.onload = function() {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+poweatherRequest.onload = function() {
+    let weatherData = JSON.parse(poweatherRequest.responseText);
     //console.log(weatherData);
    
 
     
     document.getElementById('temp2').innerHTML = weatherData.list[0].main.temp.toFixed(0) +"&#8457";   
+     
+
+    let icon = "https://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";
+    let des = weatherData.list[0].weather[0].description;  
+
+    document.getElementById('cc-img3').setAttribute('src', icon);
+    document.getElementById('cc-img3').setAttribute('alt', des);
 
 }
